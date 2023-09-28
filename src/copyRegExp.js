185609aa094606fbe26e8e094b84d3a3e4599edd
emayom <emayom@naver.com>
2023-09-28 22:27:25 +0900

    fix: copyRegExp source가 g 옵션일 경우 lastIndex도 복사하도록 수정

--- src/copyRegExp.js
+++ src/copyRegExp.js
@@ -2,6 +2,12 @@
  * @param {Object} source
  * @returns {Object} 복사된 `RegExp`를 리턴한다.
  */
- const copyRegExp = source => new RegExp(source.source, source.flags);
+ const copyRegExp = source => {
+    const result = new RegExp(source.source, source.flags);
+    if(source.global){
+        result.lastIndex = source.lastIndex;
+    }
+    return result;
+ }
  
 export default copyRegExp;
