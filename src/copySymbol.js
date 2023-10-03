1707ee4e4cf8944dd742568f1f5e1d3d5ada2a27
emayom <emayom@naver.com>
2023-10-03 23:10:21 +0900

    feat: copySymbol 기능 구현

--- src/copySymbol.js
+++ src/copySymbol.js
@@ -3,6 +3,6 @@
  * @param {Object} source
  * @returns {Object} 복사된 `Symbol`을 리턴한다.
  */
-const copySymbol = source => {};
+const copySymbol = source => Object(source.valueOf());
 
 export default copySymbol;
