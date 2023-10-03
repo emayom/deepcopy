30e93eba7f7bd684804bbc2de757417cf46eda5a
emayom <emayom@naver.com>
2023-10-04 00:53:19 +0900

    feat: copyTypedArray 기능 구현

--- src/copyTypedArray.js
+++ src/copyTypedArray.js
@@ -1,8 +1,13 @@
+import cloneArrayBufferView from "./cloneArrayBufferView";
 /**
  * `typedArray`을 복사한다. 
  * @param {Object} source
+ * @param {boolean} isDeep 
  * @returns {Object} 복사된 `typedArray`를 리턴한다.
  */
-const copyTypedArray = source => {};
+const copyTypedArray = (source, isDeep) => {
+    return cloneArrayBufferView(source, isDeep)
+            .map((val, idx)=> val = source[idx]);
+};
 
 export default copyTypedArray;
