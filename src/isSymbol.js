8cb591ded1a2312c5b830f95a4febed5c73a2e1c
emayom <emayom@naver.com>
2023-10-03 22:58:49 +0900

    fix: 오타 수정

--- src/isSymbol.js
+++ src/isSymbol.js
@@ -3,6 +3,6 @@
  * @param {*} value
  * @returns {boolean} `value`가 Symbol이라면 `true`를, 아니라면 `false`를 리턴한다.
  */
-const isSymbol = c => value?.constructor === Symbol;
+const isSymbol = value => value?.constructor === Symbol;
 
 export default isSymbol;
